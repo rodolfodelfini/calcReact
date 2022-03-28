886faa87fd0be8904590f2dd83f4c8b852b6873a
rodolfodelfini <rodolfodelfini@gmail.com>
2022-03-28 00:02:24 -0300

    main

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
-import logo from './logo.svg';
+//import Head from 'next/head'
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [equation, setEquation] = useState(0);
+
+  function calculator(item){
+    if(typeof item == 'number') {
+      if(equation == 0) {
+        setEquation(item)
+      }else {
+        setEquation(''+equation+item)
+      }
+    }
+
+    if(item == '*' ||  item == '/' ||  item == '-'  ||  item == '+') {
+      setEquation(equation+item)
+    }
+
+    //clean screen
+    if(item === 'C') {
+      setEquation(0)
+    }
+
+    if(item == '=') {
+      setEquation(eval(equation))
+    }
+  }
+
   return (
       <div className='container'>
           <div className='box'>
             <div className='calc'>
-              <input className='display' type='text' value='1' />
+              <input className='display' type='text' value={equation} />
               <div  className='buttons'>
-                <span>%</span>
-                <span>CE</span>
-                <span>C</span>
-                <span>'/-'</span>
-                <span>7</span>
-                <span>8</span>
-                <span>9</span>
-                <span>X</span>
-                <span>4</span>
-                <span>5</span>
-                <span>6</span>
-                <span>./.</span>
-                <span>1</span>
-                <span>2</span>
-                <span>3</span>
-                <span>-</span>
-                <span>.</span>
-                <span>0</span>
-                <span className='equal'>=</span>
+                <span onClick={() => calculator('%')} >%</span>
+                <span onClick={() => calculator('C')} >C</span>
+                <span onClick={() => calculator('<-')} >{`<-`}</span>
+                <span onClick={() => calculator('+')} >+</span>
+
+                <span onClick={() => calculator(7)} >7</span>
+                <span onClick={() => calculator(8)} >8</span>
+                <span onClick={() => calculator(9)} >9</span>
+                <span onClick={() => calculator('-')} >-</span>
+
+                <span onClick={() => calculator(4)} >4</span>
+                <span onClick={() => calculator(5)} >5</span>
+                <span onClick={() => calculator(6)} >6</span>
+                <span onClick={() => calculator('*')} >x</span>
+
+                <span onClick={() => calculator(1)} >1</span>
+                <span onClick={() => calculator(2)} >2</span>
+                <span onClick={() => calculator(3)} >3</span>
+                <span onClick={() => calculator('/')} >/</span>
+
+                <span onClick={() => calculator('.')} >.</span>
+                <span onClick={() => calculator(0)} >0</span>
+                <span onClick={() => calculator('=')}  className='equal'>=</span>
               </div>
             </div>
           </div>
